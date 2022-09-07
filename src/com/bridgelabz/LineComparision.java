d944ce8a03efcf23394d46d27cc754f68d650dcb
Radhika <reddypallyradhika@gmail.com>
2022-09-07 16:28:06 +0530

    Added:Line Comparison Computation Program:UC1

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -3,6 +3,8 @@
  */
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 /**
  * @author hp
  *
@@ -14,7 +16,20 @@ public class LineComparision {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		double length;
+	    Scanner sc = new Scanner(System.in);  
+		System.out.print("Enter the value of x1: ");  
+		double x1 = sc.nextDouble();  
+		System.out.print("Enter the value of y1: ");  
+		double y1 = sc.nextDouble();  
+		System.out.print("Enter the value of x2: ");  
+		double x2 = sc.nextDouble(); 
+		System.out.print("Enter the value of y2: ");  
+		double y2 = sc.nextDouble();
+		length =Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));	
+		
+        System.out.println("Length of Line ="+length);
+	
 	}
 
 }
