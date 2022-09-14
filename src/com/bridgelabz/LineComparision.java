b9a658e391479189851c1af0d958544e68f9cfce
Radhika <reddypallyradhika@gmail.com>
2022-09-14 15:37:09 +0530

    Added: UC4: Line Comparison Problem: Using oops concept.

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -10,7 +10,10 @@ import java.util.Scanner;
  *
  */
 public class LineComparision {
-
+	double distance1,distance2,d1,d2;
+	double x1,y1,x2,y2,x3,y3,x4,y4;
+	//int 
+	double compare;
 	/**
 	 * @param args
 	 */
@@ -25,11 +28,17 @@ public class LineComparision {
 	 * So that I know one line is equal, greater or less than the other line. 
 	 * - Using Java compareTo method to compare 2 Lengths is preferable.
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		double length1,length2;
-	    Scanner sc = new Scanner(System.in);  
-		System.out.println("Enter the value of x1:");  
+	/*
+	 * UC4
+	 * Use Java Object Oriented Programming Concepts of Line and Point 
+	 * as well as equals and compareTo methods. 
+	 * - Using Java compareTo method to compare 2 Lengths is preferable
+	 */
+		 public void line()
+		 {
+
+	    Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of x1: ");
 		double x1 = sc.nextDouble();  
 		System.out.println("Enter the value of y1: ");  
 		double y1 = sc.nextDouble();  
@@ -37,40 +46,67 @@ public class LineComparision {
 		double x2 = sc.nextDouble(); 
 		System.out.println("Enter the value of y2: ");  
 		double y2 = sc.nextDouble();  
-		length1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 		System.out.println("(x1,y1)&(x2,y2) are coordinates to find the length of Line1");
 		System.out.println(" ");
+		
 		System.out.println("Enter the value of x3: ");  
 		double x3= sc.nextDouble();  
-		System.out.print("Enter the value of y3: ");  
+		System.out.println("Enter the value of y3: ");  
 		double y3= sc.nextDouble();  
 		System.out.println("Enter the value of x4: ");  
 		double x4= sc.nextDouble(); 
 		System.out.println("Enter the value of y4: ");  
-		double y4= sc.nextDouble();
-		length2 = Math.sqrt((x4-x3)*(x4-x3)+(y4-y3)*(y4-y3));
+		double y4= sc.nextDouble(); 
+	    System.out.println(" ");
 		System.out.println("(x3,y3)&(x4,y4) are coordinates to find the length of Line2");
 		System.out.println(" ");
-       System.out.println("Length of Line1 ="+length1);
-       System.out.println("Length of Line2 ="+length2);
-        Double l1= Double.valueOf(length1);
-        Double l2= Double.valueOf(length2);
-        int compare=l1.compareTo(l2); 
-        System.out.println(" ");
-        if(compare > 0)
-         {
-        	 
-        	 System.out.println("Line1 is greater than Line2");
-         }
-        else if(compare < 0)
+		d1 = (Double)Math.sqrt(((x2 - x1) * (x2 - x1) ) + ((y2 - y1) *(y2 - y1)));
+		d2= (Double)Math.sqrt(((x4 - x3) * (x4 - x3) ) + ((y4 - y3) *(y4 - y3)));
+		System.out.println("length of line is:"+d1+"and"+d2);
+		 }
+	/*	public void distance()
+		 {
+	    distance1 = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+	//	d1 = (Double)Math.sqrt(((x2 - x1) * (x2 - x1) ) + ((y2 - y1) *(y2 - y1)));
+	//	System.out.println(d1);
+		d1= Double.valueOf(distance1);
+		distance2 = Math.sqrt((x4-x3)*(x4-x3)+(y4-y3)*(y4-y3));
+	//	d2= (Double)Math.sqrt(((x4 - x3) * (x4 - x3) ) + ((y4 - y3) *(y4 - y3)));
+		System.out.println("length of line is:"+d1+"and"+d2);
+		d2= Double.valueOf(distance2);
+		
+		 }*/
+		public void compareTo()
+		{
+			
+		        if(d1 > d2)
+		         {
+		        	 
+		        	 System.out.println("Line1 is greater than Line2");
+		         }
+		        else if(d1<d2)
+		        {
+		        
+		       	 System.out.println("Line1 is less than Line2");
+		        }
+		        else
+		        {
+		       	 System.out.println("Line1 & Line2 are equal");
+		        }
+			}
+
+        public static void main(String[] args) 
         {
-        
-       	 System.out.println("Line1 is less than Line2");
+    		// TODO Auto-generated method stub
+        	LineComparision LC1= new LineComparision();
+        	
+        	LC1.line();
+        	//LC1.distance();
+        	LC1.compareTo();
+        	
+			
         }
-        else
-        {
-       	 System.out.println("Line1 & Line2 are equal");
-        }
+		   	        
 	}
 
-}
+
